add7a6e5f02604799fa83834a2692ba1d67ced2e
dumidu1998 <dumiduraj@gmail.com>
2021-05-26 01:27:36 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 import { useState, useRef, useCallback } from 'react';
 import './App.css';
 import Card from './components/Card';
+import Footer from './components/Footer';
 import Scroll from './components/Scroll';
 import useBookSearch from './useBookSearch';
 // import { Switch } from '@headlessui/react';
@@ -43,26 +44,16 @@ function App() {
       <div class="flex">
         <form>
           <span class="text-sm border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Enter Github Username:</span>
-          <input name="field_name" class=" border-2 rounded-r px-4 py-2 w-56" type="text"
+          <input name="field_name" class=" ml-4 lg:ml-0 border-2 rounded-r px-4 py-2 w-56" type="text"
             value={input} onChange={event => setinput(event.target.value)} placeholder="dumidu1998" />
-          <button onClick={clickk} className="bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded">Click to Load</button>
+          <button onClick={clickk} className="bg-indigo-600 hover:bg-blue-dark ml-4 text-white font-bold py-3 px-6 rounded">Click to Load</button>
         </form>
       </div>
 
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 px-4">
         {repos.map(repo => (<Card h2={repo.name} img={repo.avatar} url={repo.url} desc={repo.desc} />))}
       </div>
-      {/* {repos.map(repo => (repo.avatar))} */}
-      {/* {repos.toString()} */}
-      {/* <div className=" w-60 h-5">
-        <div className="flex flex-col">
-          Id: <input type="text" className=" bg-yellow-300" value="1" />
-          Name: <input type="text" className="bg-red-500" value="Dumidu Kasun" />
-          Age: <input type="text" className="bg-blue-500" value="21" />
-          City: <input type="text" className="bg-green-500" value="Anuradhapura" />
-        </div>
-      </div> */}
-
+      <Footer />
     </>
 
   )

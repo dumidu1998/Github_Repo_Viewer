7c50306273209c802fde3a92de2653f2ca61b6d7
dumidu1998 <dumiduraj@gmail.com>
2021-05-26 02:54:04 +0530

    Style edit responsive

--- src/App.js
+++ src/App.js
@@ -20,38 +20,33 @@ function App() {
     setRepos([]);
     Axios.get('https://api.github.com/users/' + input + '/repos').then(
       (res) => {
-        // console.log(res);
-        // if (res.data.message == 'Not Found') alert("User Not found");
+        if (res.data.length == 0) alert("No data found");
         res.data.map(
           d => {
-            // console.log(d.name);
-            // setRepos([...repos, d.name])
             setRepos(oldArray => [...oldArray, { name: d.name, avatar: d.owner.avatar_url, url: d.html_url, desc: d.full_name }]);
-
           }
         )
       })
       .catch(err => {
         alert('User ' + err.response.data.message);
       })
-
   }
-
-
 
   return (
     <>
-      <div class="flex">
-        <form>
-          <span class="text-sm border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Enter Github Username:</span>
-          <input name="field_name" class=" ml-4 lg:ml-0 border-2 rounded-r px-4 py-2 w-56" type="text"
-            value={input} onChange={event => setinput(event.target.value)} placeholder="dumidu1998" />
-          <button onClick={clickk} className="bg-indigo-600 hover:bg-blue-dark ml-4 text-white font-bold py-3 px-6 rounded">Click to Load</button>
-        </form>
-      </div>
+      <div className="min-h-screen">
+        <div class="flex">
+          <form>
+            <span class="text-sm border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Enter Github Username:</span>
+            <input name="field_name" class=" ml-4 lg:ml-0 border-2 rounded-r px-4 py-2 w-56" type="text"
+              value={input} onChange={event => setinput(event.target.value)} placeholder="dumidu1998" />
+            <button onClick={clickk} className="bg-indigo-600 hover:bg-blue-dark ml-1 text-white font-bold py-3 px-6 rounded">Click to Load</button>
+          </form>
+        </div>
 
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 px-4">
-        {repos.map(repo => (<Card h2={repo.name} img={repo.avatar} url={repo.url} desc={repo.desc} />))}
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 px-4 ">
+          {repos.map(repo => (<Card h2={repo.name} img={repo.avatar} url={repo.url} desc={repo.desc} />))}
+        </div>
       </div>
       <Footer />
     </>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div class="container mx-auto px-6 fixed bottom-0">
-            <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+        <div class="container mx-auto px-6">
+            <div class="-mt-16 border-t-2 border-gray-300 flex flex-col items-center">
                 <div class="sm:w-2/3 text-center py-6">
                     <p class="text-sm text-black font-bold mb-2">
                         © 2021 by Dumidu Kasun Rajakaruna <a href="https://www.github.com/dumidu1998" className='text-blue-900'>@dumidu1998</a>
